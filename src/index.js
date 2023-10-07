4b2295481369d1432026fdf5795851bc4fb21909
ahmetkocar <ahmetkocar@gmail.com>
2023-10-07 12:43:34 +0300

    Implement dynamic navbar styling based on active section

--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@ import "./style.css";
 
 const navItems = document.querySelectorAll("nav a");
 
-navItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    navItems.forEach((navItem) => {
-      navItem.classList.remove("active");
+function setActiveSection() {
+  if (
+    window.innerHeight + Math.round(window.scrollY) >=
+    document.body.offsetHeight
+  ) {
+    navItems.forEach((item) => {
+      item.classList.remove("active");
     });
 
-    item.classList.add("active");
+    navItems[2].classList.add("active");
+  } else if (window.scrollY > document.body.offsetHeight * 0.2) {
+    navItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    navItems[1].classList.add("active");
+  } else {
+    navItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    navItems[0].classList.add("active");
+  }
+}
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    setActiveSection();
   });
 });
+
+window.addEventListener("scroll", setActiveSection);
